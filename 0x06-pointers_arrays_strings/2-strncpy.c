1c007630fbfcfe2fd965b19cf3ff8f9695139b32
Mbogo47 <njokiivy@gmail.com>
2023-03-02 03:13:56 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
+ * Return: Always 0 (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
+
+}
+
