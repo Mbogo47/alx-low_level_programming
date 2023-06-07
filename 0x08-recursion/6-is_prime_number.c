15279d8b4c5450c3bac89d02413d10e60014cfec
Mbogo47 <njokiivy@gmail.com>
2023-06-07 10:42:27 -0700

    Question 7

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,37 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * is_prime_number - Checks if a number is prime
- * @n: The number to be checked
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_number(int n)
+  *evaluate_num - recursion loop
+  *@num : number
+  *@iterator : number to iterate
+  *Return: return either 1 or 0
+ **/
+
+int evaluate_num(int num, int iterator)
 {
-	/* Base cases */
-	if (n <= 1)
-		return 0;
-	if (n <= 3)
-		return 1;
-
-	/* Check if n is divisible by any number from 2 to sqrt(n) */
-	int i;
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return 0;
-	}
-
-	return 1;
+	if (iterator == num - 1)
+		return (1);
+	else if (num % iterator == 0)
+		return (0);
+	if (num % iterator != 0)
+		return (evaluate_num(num, iterator + 1));
+	return (0);
 }
 
+/**
+  *is_prime_number - evaluate whether a number is prime or not
+  *@num : input digit
+  *Return: returns 1 if number is prime and 0 if number is not
+**/
+
+int is_prime_number(int num)
+{
+	int iterator;
+
+	iterator = 2;
+
+	if (num < 2)
+		return (0);
+	if (num == 2)
+		return (1);
+	return (evaluate_num(num, iterator));
+}
