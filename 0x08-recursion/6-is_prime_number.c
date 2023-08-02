1df0a9a36bc70f0fc32ab3d9e0b438b4cee1283d
Mbogo47 <97885622+Mbogo47@users.noreply.github.com>
2023-08-02 20:30:19 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,14 +1,15 @@
-i#include "main.h"
+#include "main.h"
 /**
   *evaluate_num - recursion loop
+  
  **/
 
 int evaluate_num(int num, int iterator)
 {
-    if (num % iterator == 0)
-		return (0);
-	else if (iterator == num - 1)
+	if (iterator == num - 1)
 		return (1);
+	else if (num % iterator == 0)
+		return (0);
 	if (num % iterator != 0)
 		return (evaluate_num(num, iterator + 1));
 	return (0);
@@ -16,16 +17,19 @@ int evaluate_num(int num, int iterator)
 
 /**
   *is_prime_number - evaluate whether a number is prime or not
+  
   *Return: returns 1 if number is prime and 0 if number is not
 **/
 
 int is_prime_number(int num)
 {
-	int iterator = 2;
-	if (num == 2)
-		return (1);
+	int iterator;
+
+	iterator = 2;
+
 	if (num < 2)
 		return (0);
+	if (num == 2)
+		return (1);
 	return (evaluate_num(num, iterator));
 }
-
