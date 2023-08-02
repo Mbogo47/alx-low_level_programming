51ed7f9e52bf2f4620961b9755d895e4e1e3b76d
Mbogo47 <97885622+Mbogo47@users.noreply.github.com>
2023-08-02 20:28:56 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-	}
-	else
+	if
 	{
 		putchar(*s);
 		_puts_recursion(s + 1);
+	}
+	else (*s == '\0')
+	{
+		putchar('\n');
 	}
 }
 
